e637efc8305514e11ff9100d39c9c5994ee10d45
hedon954 <171725713@qq.com>
2025-04-14 11:39:16 +0800

    feat(channel): seperate channel into pairs

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ mod mpsc;
 mod oneshot;
 mod spinlock;
 
-pub use mpsc::*;
-pub use oneshot::*;
+pub use mpsc::{unbounded, Receiver as MPSCReceiver, Sender as MPSCSender};
+pub use oneshot::{channel, Receiver as OneShotReceiver, Sender as OneShotSender};
 pub use spinlock::*;
--- src/oneshot.rs
+++ src/oneshot.rs
@@ -1,61 +1,58 @@
 use std::{
     cell::UnsafeCell,
     mem::MaybeUninit,
-    sync::atomic::{AtomicBool, Ordering},
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
 };
 
-pub struct Channel<T> {
+pub struct Sender<T> {
+    channel: Arc<Channel<T>>,
+}
+
+pub struct Receiver<T> {
+    channel: Arc<Channel<T>>,
+}
+
+struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
-    in_use: AtomicBool,
     ready: AtomicBool,
 }
 
 unsafe impl<T> Sync for Channel<T> where T: Send {}
 
-impl<T> Channel<T> {
-    pub fn new() -> Self {
-        Self {
-            message: UnsafeCell::new(MaybeUninit::uninit()),
-            in_use: AtomicBool::new(false),
-            ready: AtomicBool::new(false),
-        }
-    }
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let a = Arc::new(Channel {
+        message: UnsafeCell::new(MaybeUninit::uninit()),
+        ready: AtomicBool::new(false),
+    });
+    (Sender { channel: a.clone() }, Receiver { channel: a })
+}
 
-    /// Panics when trying to send more than one message.
-    pub fn send(&self, message: T) {
-        if self.in_use.swap(true, Ordering::Relaxed) {
-            panic!("can't send more the one message")
-        }
-        unsafe {
-            // Safety: Only call this once!
-            (*self.message.get()).write(message);
-        }
-        self.ready.store(true, Ordering::Release);
-    }
-
-    pub fn is_ready(&self) -> bool {
-        self.ready.load(Ordering::Relaxed)
-    }
-
-    pub fn receive(&self) -> T {
-        if !self.ready.swap(false, Ordering::Acquire) {
-            panic!("no message available")
-        }
-        // Safety: We've just checked (and reset) the ready flag
-        unsafe { (*self.message.get()).assume_init_read() }
+impl<T> Sender<T> {
+    /// This never panics. :)
+    pub fn send(self, message: T) {
+        unsafe { (*self.channel.message.get()).write(message) };
+        self.channel.ready.store(true, Ordering::Relaxed);
     }
 }
 
-impl<T> Default for Channel<T> {
-    fn default() -> Self {
-        Self::new()
+impl<T> Receiver<T> {
+    pub fn is_ready(&self) -> bool {
+        self.channel.ready.load(Ordering::Relaxed)
+    }
+
+    pub fn receive(self) -> T {
+        if !self.channel.ready.load(Ordering::Acquire) {
+            panic!("no message available!");
+        }
+        unsafe { (*self.channel.message.get()).assume_init_read() }
     }
 }
 
 impl<T> Drop for Channel<T> {
     fn drop(&mut self) {
-        // 1. no init
-        // 2. init but no use
         if *self.ready.get_mut() {
             unsafe {
                 self.message.get_mut().assume_init_drop();
@@ -72,21 +69,21 @@ mod tests {
 
     #[test]
     fn one_thread_should_work() {
-        let channel = Channel::new();
-        channel.send(1);
-        assert!(channel.is_ready());
-        assert_eq!(channel.receive(), 1);
+        let (sender, receiver) = channel();
+        sender.send(1);
+        assert!(receiver.is_ready());
+        assert_eq!(receiver.receive(), 1);
     }
 
     #[test]
     fn multi_threads_should_work() {
-        let channel = Channel::new();
+        let (sender, receiver) = channel();
         thread::scope(|s| {
             s.spawn(|| {
                 let load;
                 loop {
-                    if channel.is_ready() {
-                        assert_eq!(channel.receive(), 1);
+                    if receiver.is_ready() {
+                        assert_eq!(receiver.receive(), 1);
                         load = true;
                         break;
                     }
@@ -95,7 +92,7 @@ mod tests {
             });
             s.spawn(|| {
                 thread::sleep(Duration::from_millis(10));
-                channel.send(1);
+                sender.send(1);
             });
         });
     }
